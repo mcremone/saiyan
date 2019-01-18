4d23701d27a68d04c3b381692f0a114fc58197a2
Matteo Cremonesi <matteoc@mac-132345.guest-network.net>
2019-01-18 12:22:00 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="saiyan",
+    version="0.0.1",
+    author="Matteo Cremonesi",
+    author_email="matteoc@fnal.gov",
+    description="Sophisticated Algorithms Implemented to make Your Analysis Nice",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcremone/saiyan.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

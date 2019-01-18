be5a05cbc35d7934d7a74ce6ed273b62d6606e4b
Matteo Cremonesi <matteoc@mac-132345.guest-network.net>
2019-01-18 16:25:30 -0600

    working out the definition of the arrays

--- saiyan/arrays/Array.py
+++ saiyan/arrays/Array.py
@@ -1,38 +1,7 @@
 import awkward
 import uproot_methods
-import numpy as np
 
-def _fast_pt(p4):
-    """ quick pt calculation for caching """
-    return np.hypot(p4.x,p4.y)
-
-def _fast_eta(p4):
-    """ quick eta calculation for caching """
-    px = p4.x
-    py = p4.y
-    pz = p4.z
-    pT = np.sqrt(px*px + py*py)
-    return np.arcsinh(pz/pT)
-
-def _fast_phi(p4):
-    """ quick phi calculation for caching """
-    return np.arctan2(p4.y,p4.x)
-
-def _fast_mass(p4):
-    """ quick mass calculation for caching """
-    px = p4.x
-    py = p4.y
-    pz = p4.z
-    en = p4.t
-    p3mag2 = (px*px + py*py + pz*pz)
-    return np.sqrt(np.abs(en*en - p3mag2))
-
-
-
-class ObjectsArray(awkward.Methods):
-
-    def __init__(self, **kwargs):
-
+def Initialize(**kwargs):
         items = kwargs
         argkeys = items.keys()
         
@@ -40,12 +9,12 @@ class ObjectsArray(awkward.Methods):
         
         if 'p4' in argkeys:
             p4 = items['p4']
-            if not isinstance(p4,uproot_methods.TLorentzVector):
-                p4 = uproot_methods.TLorentzVector.from_cartesian(p4[:,0],p4[:,1],
+            if not isinstance(p4,uproot_methods.TLorentzVectorArray):
+                p4 = uproot_methods.TLorentzVectorArray.from_cartesian(p4[:,0],p4[:,1],
                                                                        p4[:,2],p4[:,3])
 
         elif 'pt' in argkeys and 'eta' in argkeys and 'phi' in argkeys and 'mass' in argkeys:
-            p4 = uproot_methods.TLorentzVector.from_ptetaphim(items['pt'],items['eta'],
+            p4 = uproot_methods.TLorentzVectorArray.from_ptetaphim(items['pt'],items['eta'],
                                                                    items['phi'],items['mass'])
             del items['pt']
             del items['eta']
@@ -53,7 +22,7 @@ class ObjectsArray(awkward.Methods):
             del items['mass']
 
         elif 'pt' in argkeys and 'eta' in argkeys and 'phi' in argkeys and 'energy' in argkeys:
-            p4 = uproot_methods.TLorentzVector.from_ptetaphi(items['pt'],items['eta'],
+            p4 = uproot_methods.TLorentzVectorArray.from_ptetaphi(items['pt'],items['eta'],
                                                                   items['phi'],items['energy'])
             del items['pt']
             del items['eta']
@@ -61,7 +30,7 @@ class ObjectsArray(awkward.Methods):
             del items['energy']
 
         elif 'px' in argkeys and 'py' in argkeys and 'pz' in argkeys and 'mass' in argkeys:
-            p4 = uproot_methods.TLorentzVector.from_xyzm(items['px'],items['py'],
+            p4 = uproot_methods.TLorentzVectorArray.from_xyzm(items['px'],items['py'],
                                                               items['pz'],items['mass'])
             del items['px']
             del items['py']
@@ -69,7 +38,7 @@ class ObjectsArray(awkward.Methods):
             del items['mass']
 
         elif 'pt' in argkeys and 'phi' in argkeys and 'pz' in argkeys and 'energy' in argkeys:
-            p4 = uproot_methods.TLorentzVector.from_cylindrical(items['pt'],items['phi'],
+            p4 = uproot_methods.TLorentzVectorArray.from_cylindrical(items['pt'],items['phi'],
                                                                      items['pz'],items['energy'])
             del items['pt']
             del items['phi']
@@ -77,7 +46,7 @@ class ObjectsArray(awkward.Methods):
             del items['energy']
 
         elif 'px' in argkeys and 'py' in argkeys and 'pz' in argkeys and 'energy' in argkeys:
-            p4 = uproot_methods.TLorentzVector.from_cartesian(items['px'],items['py'],
+            p4 = uproot_methods.TLorentzVectorArray.from_cartesian(items['px'],items['py'],
                                                                    items['pz'],items['energy'])
             del items['px']
             del items['py']
@@ -85,7 +54,7 @@ class ObjectsArray(awkward.Methods):
             del items['energy']
 
         elif 'p' in argkeys and 'theta' in argkeys and 'phi' in argkeys and 'energy' in argkeys:
-            p4 = uproot_methods.TLorentzVector.from_spherical(items['p'],items['theta'],
+            p4 = uproot_methods.TLorentzVectorArray.from_spherical(items['p'],items['theta'],
                                                                    items['phi'],items['energy'])
             del items['p']
             del items['theta']
@@ -93,16 +62,36 @@ class ObjectsArray(awkward.Methods):
             del items['energy']
 
         elif 'p3' in argkeys and 'energy' in argkeys:
-            p4 = uproot_methods.TLorentzVector.from_p3(items['p3'],items['energy'])
+            p4 = uproot_methods.TLorentzVectorArray.from_p3(items['p3'],items['energy'])
             del items['p3']
             del items['energy']
 
         else:
             raise Exception('No valid definition of four-momentum')
 
-        self = p4
-        for arg in argkeys: self[arg] = items[arg]
+        out = p4
+        for name, value in items.items():
+            out[name] = value
 
+        return out
 
+class Dangerousness(object):
     def __getattr__(self, name):
         return self[name]
+
+class ObjectsArray(Dangerousness,awkward.Methods):
+    def __init__(self, **kwargs):
+        self = Initialize(**kwargs)
+        # adding dangerousness to the ObjectArray
+        self.__class__ = Methods.mixin(Dangerousness, self.__class__)
+
+class CollectionsArray(Dangerousness,awkward.Methods):
+    def __init__(self, **kwargs):
+        self = Initialize(**kwargs)
+        # adding dangerousness to the JaggedArray
+        self.__class__ = Methods.mixin(Dangerousness, self.__class__)
+        # adding dangerousness to the ObjectArray inside it
+        self.content.__class__ = Methods.mixin(Dangerousness,, self.content.__class__)
+
+
+
